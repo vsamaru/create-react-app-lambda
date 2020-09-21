36ca3bfd2bfa055bfc4dc8674cb3ffdc0a4060bd
vsamaru <vsamaru@gmail.com>
2020-09-21 10:51:56 +0300

    Update async-dadjoke.js

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -8,7 +8,7 @@ export async function handler(event, context) {
     const data = response.data
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.joke })
+      body: JSON.stringify({ msg: data.joke, x:88 })
     }
   } catch (err) {
     console.log(err) // output to netlify function log
